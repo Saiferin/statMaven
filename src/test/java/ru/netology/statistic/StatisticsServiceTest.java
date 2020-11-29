04aa2d10310cff7a2c6010d1e41d15f29a75b25d
Saiferin <mr.badikov@bk.ru>
2020-11-29 17:31:38 +0300

    n
    s

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -1,7 +1,7 @@
 package ru.netology.statistic;
 
 import org.junit.jupiter.api.Test;
-
+import ru.netology.statistic.StatisticsService;
 import static org.junit.jupiter.api.Assertions.*;
 
 class StatisticsServiceTest {
